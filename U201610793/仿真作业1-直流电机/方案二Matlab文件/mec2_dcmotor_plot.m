5706541d877eb6a995f0c5b05a691638d538e717
Zzw <48195029+GitZzw@users.noreply.github.com>
2019-03-08 19:56:58 +0800

    Add files via upload
    降压启动&能耗制动

---
+++ U201610793/仿真作业1-直流电机/方案二Matlab文件/mec2_dcmotor_plot.m
@@ -0,0 +1,49 @@
+% 参照Mathworks官方例程修改
+% 陈冰 
+% 2019�?2�?
+% 显示电机的电流，转�?�和负载转矩波形
+
+% Reuse figure if it exists, else create new figure
+try
+    figure(h1_mec2_dcmotor)
+catch
+    h1_mec2_dcmotor=figure('Name', 'mec2_dcmotor');
+end
+
+% Generate simulation results if they don't exist
+if(~exist('simlog_mec2_dcmotor','var'))
+    sim('mec2_dcmotor')
+end
+
+% Get simulation results
+temp_moti = simlog_mec2_dcmotor.DC_Motor.Rotor_Resistance.i.series;
+temp_mott = simlog_mec2_dcmotor.Load_Torque.t.series;
+temp_motn = simlog_mec2_dcmotor.DC_Motor.Rotational_Electromechanical_Converter.w.series;
+
+% Plot results
+ah(1) = subplot(3,1,1);
+plot(temp_moti.time,temp_moti.values,'LineWidth',1);
+set(gca,'YLim',[-2000 1500]);
+grid on
+title('Motor Current');
+ylabel('Current (A)');
+
+ah(2) = subplot(3,1,2);
+plot(temp_motn.time,temp_motn.values*9.55,'LineWidth',1);
+set(gca,'YLim',[-800 2000]);
+grid on
+title('Motor Speed');
+ylabel('Speed (rpm)');
+
+ah(3) = subplot(3,1,3);
+plot(temp_mott.time,-temp_mott.values,'LineWidth',1);
+set(gca,'YLim',[-500 500]);
+grid on
+title('Load Torque');
+xlabel('Time (s)');
+ylabel('Torque (N*m)');
+
+linkaxes(ah,'x');
+
+% Remove temporary variables
+clear temp_moti temp_mott temp_motn ah
